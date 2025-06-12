831f5973c6412d1a4d8744bc5467576595221e01
djordjejelicki007 <djordjejelicki007@gmail.com>
2025-06-12 12:41:52 +0200

    adding menuparser controller

---
+++ MenuParser.API/Controllers/MenuParserController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using MenuParser.API.Requests.MenuRequests;
+using MenuParser.Application.Menus.Commands.CreateMenu;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MenuParser.API.Controllers
+{
+    [ApiController]
+    [Route("api/menu")]
+    public class MenuParserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public MenuParserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMenu(CreateMenuRequest request)
+        {
+            var id = await _mediator.Send(new CreateMenuCommand
+            {
+                Name = request.Name!
+            });
+            return Ok(new { Id = id });
+        }
+    }
+}
---
+++ MenuParser.API/Requests/MenuRequests/CreateMenuRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace MenuParser.API.Requests.MenuRequests
+{
+    public class CreateMenuRequest
+    {
+        public String? Name { get; set; }
+    }
+}
